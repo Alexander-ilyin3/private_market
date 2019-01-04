1c40c2d2da5f18d663d710e668c89f5097f960b6
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2019-01-04 16:01:05 +0200

    Add path to api, add login reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import loginData from './loginData';
+
+export default combineReducers({
+    loginData,
+})
---
+++ src/reducers/loginData.js
@@ -0,0 +1,19 @@
+const emptyState = {
+    isLoggedIn: false,
+    user: {},
+};
+
+let initialState;
+
+localStorage['appState'] && (initialState = JSON.parse(localStorage['appState']));
+
+export default function loginData(state = initialState || emptyState, action) {
+    if(action.type === 'LOG_IN'){
+        localStorage["appState"] = JSON.stringify(action.payload);
+        return action.payload;
+    }
+    if(action.type === 'LOG_OUT'){
+        localStorage["appState"] = JSON.stringify(emptyState);
+        return emptyState;
+    }
+}
