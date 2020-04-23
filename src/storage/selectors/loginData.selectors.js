e8bfa1c707b6c109007781bb6e2a21f5dca20ad7
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-04-23 20:12:59 +0300

    Update loginData reducer and selector

--- src/storage/selectors/loginData.selectors.js
+++ src/storage/selectors/loginData.selectors.js
@@ -1,5 +1,6 @@
 const loginData = state => state.loginData || {}
 const isLoggedIn = state => loginData(state).isLoggedIn
+const error = state => loginData(state).error
 
-export { loginData, isLoggedIn }
-export default { loginData, isLoggedIn }
+export { loginData, isLoggedIn, error }
+export default { loginData, isLoggedIn, error }
