43a33b5e761f1a12cb950cac9d7f846edb89a844
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-04-23 13:32:30 +0300

    Fix FormGroup path

--- src/components/parts/ReactiveForm/index.js
+++ src/components/parts/ReactiveForm/index.js
@@ -1,7 +1,7 @@
 import { Control } from './Control'
 import { ControlGroup } from './ControlGroup'
 import FormControl from './FormControl'
-import FormGroup from './FormGroup'
+import FormGroup from './FormContainer'
 
 export {
   Control,
