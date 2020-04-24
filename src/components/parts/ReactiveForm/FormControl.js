6caa4ee6d5e0d31dc40264c13ab1695edbdd233e
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-04-24 15:39:37 +0300

    Add prop types to form control

--- src/components/parts/ReactiveForm/FormControl.js
+++ src/components/parts/ReactiveForm/FormControl.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-export default function () {
-  return (
-    <></>
-  )
+import PropTypes from 'prop-types'
+
+const FormControl = ({ name, render }) => <>{render(name)}</>
+
+export default FormControl
+
+FormControl.propTypes = {
+  name: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
 }
