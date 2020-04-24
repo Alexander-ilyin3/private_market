736ede2454d40f1774a8dad78569567037d88de2
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-04-24 15:46:19 +0300

    Update recovery container

--- src/components/pages/Guest/pages/Recovery/Recovery.container.js
+++ src/components/pages/Guest/pages/Recovery/Recovery.container.js
@@ -1,7 +1,6 @@
 import { withStyles } from '@material-ui/core'
-import { withRouter } from 'react-router-dom'
 
 import Recovery from './Recovery'
 import { styles } from './Recovery.styles'
 
-export default withRouter(withStyles(styles)(Recovery))
+export default withStyles(styles)(Recovery)
