c17f499f8d1dac925c59b47f1c0e976bed36988e
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-04-27 05:26:44 +0300

    Rework Form control

--- src/components/parts/ReactiveForm/FormControl.js
+++ src/components/parts/ReactiveForm/FormControl.js
@@ -1,8 +1,38 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 
-const FormControl = ({ name, render }) => <>{render(name)}</>
+import { FormContext } from './formContext'
+import MemoizedControl from './MemoizedControl'
+
+const FormControl = ({ name, render }) => {
+  const { controls } = useContext(FormContext)
+  const control = controls[name]
+  const {
+    value,
+    errors,
+    touched,
+    valid,
+    invalid,
+    meta,
+  } = control
+  const props = {
+    key: name,
+    render: render || control.render,
+    value,
+    errors,
+    touched,
+    valid,
+    invalid,
+    onInput: controls[name].setValue,
+    onBlur: controls[name].blur,
+    meta,
+  }
+  return (
+    <MemoizedControl
+      {...props}
+    />
+  )
+}
 
 export default FormControl
 
