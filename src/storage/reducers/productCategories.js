3cd3bcde263d63afd2f96cd7fd69d1125cff8085
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-04-28 02:53:03 +0300

    Set default productCategories state

--- src/storage/reducers/productCategories.js
+++ src/storage/reducers/productCategories.js
@@ -1,6 +1,15 @@
 import { SET_PRODUCT_CATEGORIES } from '../constants/productCategories.constants'
 
-export default function productCategories(state = 0, action) {
+const defaultState = ({
+  categories: [],
+  config: {
+    page: 0,
+    rowsPerPage: 5,
+    count: 0,
+  },
+})
+
+export default function productCategories(state = defaultState, action) {
   if (action.type === SET_PRODUCT_CATEGORIES) {
     return action.payload
   }
