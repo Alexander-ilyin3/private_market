beeec228ecda4c4fd3c8d2136e2bbf4807a4be51
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-04-28 02:53:25 +0300

    Connect product categories to the redux

--- src/components/pages/products/ProductCategories/ProductsCetegories.container.js
+++ src/components/pages/products/ProductCategories/ProductsCetegories.container.js
@@ -1,4 +1,15 @@
-import Products from './ProductsCetegories'
+import { connect } from 'react-redux'
+import { productCategories } from 'storage/selectors/productCategories.selector'
+import { getProductCategories } from 'services/api/products.service'
 
+import ProductCategories from './ProductsCetegories'
 
-export default Products
+const mapStateToProps = state => ({
+  productCategories: productCategories(state),
+})
+
+const mapDispatchToProps = dispatch => ({
+  getProductCategories: params => dispatch(getProductCategories(params)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductCategories)
