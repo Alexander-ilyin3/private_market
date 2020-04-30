970a192c7372106ffb4e88d0ed4b6e6842e89858
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-04-30 23:25:02 +0300

    Implemented vendor seector

--- src/storage/selectors/products.selector.js
+++ src/storage/selectors/products.selector.js
@@ -5,3 +5,5 @@ export const productsData = state => state.productsData || {}
 export const products = state => productsData(state).products || []
 
 export const config = state => productsData(state).config || {}
+
+export const vendors = state => productsData(state).vendors || {}
