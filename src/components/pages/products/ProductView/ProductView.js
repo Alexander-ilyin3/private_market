737c232c7fbbf28c3d6c4d5231e445372f0dfc1e
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-05-07 18:29:07 +0300

    Create product view page

---
+++ src/components/pages/products/ProductView/ProductView.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+
+import {
+  Paper,
+  Grid,
+  Typography,
+  Chip,
+  Button,
+  TextField,
+} from '@material-ui/core'
+import { CheckCircle } from '@material-ui/icons'
+
+export default class ProductView extends Component {
+  componentDidMount() {
+    const { getData, match } = this.props
+    const { id } = match.params
+    getData(id)
+  }
+
+  render() {
+    console.log(this.props)
+    const { classes, product = {} } = this.props
+    const { data = {} } = product
+    const {
+      barcode,
+      category_name,
+      image,
+      name,
+      price,
+    } = data
+    return (
+      <Paper className={classes.ProductPage}>
+        <Typography variant='h5'>
+          {name}
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item sm={12} md={4}>
+            <div className={classes.imageBlock}>
+              <img alt='Здесь должна была быть картинка' src={image} />
+            </div>
+          </Grid>
+          <Grid item sm={12} md={8}>
+            <Grid container spacing={3} className={classes.detailItem}>
+              <Grid item xs={12} sm={7}>
+                <Typography>Цена:</Typography>
+                <Typography variant='h4'>{price}</Typography>
+              </Grid>
+              <Grid item xs={12} sm={5}>
+                <Typography>Артикул:</Typography>
+                <Typography variant='caption'>{barcode}</Typography>
+              </Grid>
+            </Grid>
+            <Grid item xs={12} className={classes.detailItem}>
+              <Typography>Категории:</Typography>
+              <Chip
+                label={category_name}
+                color='primary'
+              />
+            </Grid>
+            <Grid item sm={12} md={8} className={classes.detailItem}>
+              <Typography>Описние:</Typography>
+              <Typography>Здесь когда ни будь будет описание</Typography>
+            </Grid>
+            <Grid container className={classes.detailItem}>
+              <Grid item sm={8} xs={12}>
+                <Typography><CheckCircle fontSize='large' color='primary' /> В наличии</Typography>
+              </Grid>
+              <Grid item sm={4} xs={12}>
+                <div className={classes.countContainer}>
+                  <TextField
+                    variant='outlined'
+                    type='number'
+                    InputProps={{
+                      endAdornment: <Button variant='contained' color='primary' className={classes.toOrderButon}>В заказ</Button>,
+                    }}
+                  />
+                </div>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    )
+  }
+}
---
+++ src/components/pages/products/ProductView/ProductViewStyles.js
@@ -0,0 +1,27 @@
+export const styles = _theme => ({
+  ProductPage: {
+    padding: 20,
+  },
+  imageBlock: {
+
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+    '& img': {
+      maxWidth: '100%',
+    },
+  },
+  detailItem: {
+    marginTop: 22,
+  },
+  countContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  toOrderButon: {
+    whiteSpace: 'nowrap',
+    marginRight: '-12px',
+  },
+})
