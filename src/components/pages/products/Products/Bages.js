7ad479866aeef91bbaa00d7bbf93dfb967ff8806
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-05-08 18:18:43 +0300

    Fix bages styles

--- src/components/pages/products/Products/Bages.js
+++ src/components/pages/products/Products/Bages.js
@@ -4,10 +4,16 @@ import { Badge } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 import classNames from 'classnames'
 
-const styles = ({ palette }) => {
+const styles = ({ palette, breakpoints }) => {
   return {
     inlineBage: {
       whiteSpace: 'nowrap',
+      '& span': {
+        transform: 'scale(1) translate(95%, -50%)',
+        [breakpoints.down('sm')]: {
+          transform: 'translate(0,-50%)',
+        },
+      },
     },
     warningBage: {
       '& span': {
@@ -20,7 +26,7 @@ const styles = ({ palette }) => {
 const mapStockBages = (props) => {
   const { value, classes } = props
   if (!value) {
-    return <Badge color='error' className={classes.inlineBage} style={{ backgroundColor: '#f39c12' }} badgeContent='Нет на складе' />
+    return <Badge color='error' className={classes.inlineBage} badgeContent='Нет на складе' />
   }
   if (value === 1) {
     return <Badge color='warning' className={classNames(classes.warningBage, classes.inlineBage)} badgeContent='Заканчивается' />
