73d3800ad0663482212595d75ea3f878134b739f
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-05-29 01:39:17 +0300

    Use link to the preorder path and cart helper

--- src/components/parts/CartIndicator/CartIndicator.js
+++ src/components/parts/CartIndicator/CartIndicator.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 import Button from '@material-ui/core/Button'
 import Typograhy from '@material-ui/core/Typography'
 
+import { preorderPath } from 'config/routes'
+import { calculateCartTotal } from 'services/cart/cartHelpers'
+
 const CartIndicator = (props) => {
   const { cart } = props
-  const totalPrice = cart.reduce((prev, current) => prev + Number(current.count) * Number(current.product.price.replace(',', '')), 0)
+  const totalPrice = calculateCartTotal(cart)
   return totalPrice ? (
     <>
-      <Button variant='contained' color='primary'>Оформить заказ</Button>
+      <Button variant='contained' color='primary' component={Link} to={preorderPath}>Оформить заказ</Button>
       <Typograhy variant='h6' style={{ margin: '0 8px' }}>Общая сумма: {totalPrice.toLocaleString()}</Typograhy>
     </>
   ) : null
