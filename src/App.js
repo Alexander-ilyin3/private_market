4d2cb5aa70b4e5e4bfca653febd952f81c210260
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-06-09 20:16:45 +0300

    Get user data when user logged in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
 import Guest from 'components/pages/Guest'
 import AppDrawer from 'components/AppDrawer'
 import { loginDataSelector } from 'storage/selectors'
+import { userData } from 'storage/selectors/userData.selector'
 import { clearCart } from 'services/cart/cartService'
 import { initApp } from 'services/appInit'
 
 const App = (props) => {
-  const { isLoggedIn, init } = props
-  if (isLoggedIn/* true */) {
-    init()
-    return (
+  const { isLoggedIn, init, user } = props
+  useEffect(() => {
+    if (isLoggedIn) {
+      init()
+    } else {
+      clearCart()
+    }
+  }, [isLoggedIn])
+
+  if (isLoggedIn) {
+    return user && user.roles ? (
       <BrowserRouter>
         <AppDrawer />
       </BrowserRouter>
-    )
+    ) : null
   }
-  clearCart()
   return (
     <Guest />
   )
@@ -26,6 +33,7 @@ const App = (props) => {
 
 const mapStateToProps = state => ({
   isLoggedIn: loginDataSelector.isLoggedIn(state),
+  user: userData(state),
 })
 
 const mapDispatchToProps = dispatch => ({
