18f8bc4ff119581dbf53dc0369291ceea203520f
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-06-09 17:36:39 +0300

    Fix conditions

--- src/components/pages/products/Products/Bages.js
+++ src/components/pages/products/Products/Bages.js
@@ -4,36 +4,36 @@ import { Badge } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 import classNames from 'classnames'
 
-const styles = ({ palette, breakpoints }) => {
-  return {
-    inlineBage: {
-      whiteSpace: 'nowrap',
-      '& span': {
-        transform: 'scale(1) translate(95%, -50%)',
-        [breakpoints.down('sm')]: {
-          transform: 'translate(0,-50%)',
-        },
+const styles = ({ palette, breakpoints }) => ({
+  inlineBage: {
+    whiteSpace: 'nowrap',
+    '& span': {
+      transform: 'scale(1) translate(95%, -50%)',
+      [breakpoints.down('sm')]: {
+        transform: 'translate(0,-50%)',
       },
     },
-    warningBage: {
-      '& span': {
-        backgroundColor: palette.warning.main,
-      },
+  },
+  warningBage: {
+    '& span': {
+      backgroundColor: palette.warning.main,
     },
-  }
-}
+  },
+})
 
 const mapStockBages = (props) => {
-  const { value, classes } = props
-  if (!value) {
-    return <Badge color='error' className={classes.inlineBage} badgeContent='Нет на складе' />
+  let { value } = props
+  const { classes } = props
+  const allowedStatuses = [1, 2, 3]
+
+  if (!allowedStatuses.includes(value)) value = 1
+
+  const bages = {
+    1: <Badge color='error' className={classes.inlineBage} badgeContent='Нет на складе' />,
+    2: <Badge color='warning' className={classNames(classes.warningBage, classes.inlineBage)} badgeContent='Заканчивается' />,
+    3: <Badge color='secondary' className={classes.inlineBage} badgeContent='В наличии' />,
   }
-  if (value === 1) {
-    return <Badge color='warning' className={classNames(classes.warningBage, classes.inlineBage)} badgeContent='Заканчивается' />
-  }
-  if (value === 2) {
-    return <Badge color='secondary' className={classes.inlineBage} badgeContent='Заканчивается' />
-  }
+  return bages[value]
 }
 
 
