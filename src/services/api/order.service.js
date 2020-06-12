bd7bebf395b6ec1ceaf97ca4482ba15f407d1a5f
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-06-12 17:09:16 +0300

    Creqate order list service

--- src/services/api/order.service.js
+++ src/services/api/order.service.js
@@ -1,7 +1,9 @@
 import {
   apiCheckoutOrderPath,
+  apiOrders,
 } from 'config/apiPath'
 import { showSnack } from 'storage/actions/snack.actions'
+import { setOrderList } from 'storage/actions/order.actions'
 
 import instance from './axiosProvider'
 
@@ -15,6 +17,40 @@ export const checkout = async (orderData) => {
         variant: 'success',
         message,
       })
+    } else {
+      showSnack({
+        variant: 'error',
+        message,
+      })
+    }
+  } catch (err) {
+    const { response = {} } = err || {}
+    const { data = {} } = response
+    const { message } = data
+    let error = ''
+    if (typeof message === 'string') {
+      error = message
+    } else {
+      error = 'Oops, something went wrong'
+    }
+    showSnack({
+      variant: 'error',
+      message: error,
+    })
+  }
+}
+
+export const getOrderList = params => async (reduce) => {
+  try {
+    const res = await instance.get(apiOrders, { params })
+    const {
+      success,
+      message,
+      orders,
+      config,
+    } = res.data
+    if (success) {
+      reduce(setOrderList({ orders, config }))
     } else {
       showSnack({
         variant: 'error',
