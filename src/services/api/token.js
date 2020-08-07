20fa126b296486fc5ca7ffa993da28cf3b027a6b
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-08-07 18:34:11 +0300

    set consists token data passing at the service

--- src/services/api/token.js
+++ src/services/api/token.js
@@ -4,6 +4,6 @@ import { apiSetTokenPath } from 'config/apiPath'
 import instance from './axiosProvider'
 
 export const setToken = async ({ userId, token }) => {
-  const res = await instance.post(apiSetTokenPath, { userId, token })
+  const res = await instance.post(apiSetTokenPath.replace(':id', userId), { token_1c: token })
   return res
 }
