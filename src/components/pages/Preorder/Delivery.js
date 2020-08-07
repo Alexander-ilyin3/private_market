03a48856f4ec9da93ffa4571ca29c6ca0eeb7494
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-08-07 20:20:38 +0300

    Disable date-time picker, pass serverside delivery data

--- src/components/pages/Preorder/Delivery.js
+++ src/components/pages/Preorder/Delivery.js
@@ -3,19 +3,19 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import Paper from '@material-ui/core/Paper'
 
-import AppDateTimePicker from 'components/parts/FormParts/DateTimePicker'
+// import AppDateTimePicker from 'components/parts/FormParts/DateTimePicker'
 import { FormControl as RFormControl } from 'components/parts/ReactiveForm'
 import DefaultInputRender from 'components/parts/FormParts/DefaultInputRrnder'
 import CheckBoxRender from 'components/parts/FormParts/CheckBoxRender'
 import SelectorRender from 'components/parts/FormParts/Selector'
 
 
-const Delivery = () => (
+const Delivery = ({ deliveryMethods }) => (
   <Paper style={{ padding: 16 }}>
     <Typography variant='h5'>
       Доставка
     </Typography>
-    <RFormControl
+    {/* <RFormControl
       name='dateTime'
       render={({
         value,
@@ -30,20 +30,18 @@ const Delivery = () => (
         )
       }
       }
-    />
-    <RFormControl
-      name='deliveryType'
-      render={props => (
-        <SelectorRender
-          {...props}
-          items={[
-            { value: 1, label: 'Новая почта' },
-            { value: 2, label: 'Интайм' },
-            { value: 3, label: 'Самовывоз' },
-          ]}
-        />
-      )}
-    />
+    /> */}
+    {deliveryMethods && deliveryMethods.length > 0 && (
+      <RFormControl
+        name='deliveryType'
+        render={props => (
+          <SelectorRender
+            {...props}
+            items={deliveryMethods.map(({ id, name }) => ({ value: id, label: name }))}
+          />
+        )}
+      />
+    )}
     <RFormControl name='toDoor' render={CheckBoxRender} />
     <RFormControl name='city' render={DefaultInputRender} />
     <RFormControl name='warehouse' render={DefaultInputRender} />
