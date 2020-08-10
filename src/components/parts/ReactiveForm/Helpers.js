30d9a193eae29b77f6d16e50b259e04798740ecd
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-08-10 18:55:59 +0300

    rework helper text function

--- src/components/parts/ReactiveForm/Helpers.js
+++ src/components/parts/ReactiveForm/Helpers.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
+const defaultMessages = {
+  required: 'Поле обязательно к заполнению',
+}
+
 export const getHelperText = (errors = {}, errorMessages) => {
+  const existErrors = Object.keys(errors).filter(key => errors[key])
   if (errorMessages) {
-    const existErrors = Object.keys(errors).filter(key => errors[key])
-    return errorMessages[existErrors[0]]
+    if (errorMessages[existErrors[0]]) {
+      return errorMessages[existErrors[0]]
+    }
+    return defaultMessages[existErrors[0]]
   }
-  return (
-    <>
-      {(errors.required && 'Поле обязательно к заполнению')}
-    </>
-  )
+  return defaultMessages[existErrors[0]]
 }
