021a5f27928be08796ed290223e885046cc1bb7f
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-08-10 21:46:48 +0300

    Remove error catch

--- src/services/api/order.service.js
+++ src/services/api/order.service.js
@@ -15,33 +15,17 @@ import instance from './axiosProvider'
 
 
 export const checkout = async (orderData) => {
-  try {
-    const res = await instance.post(apiCheckoutOrderPath, orderData)
-    const { success, message } = res.data
-    if (success) {
-      showSnack({
-        variant: 'success',
-        message,
-      })
-    } else {
-      showSnack({
-        variant: 'error',
-        message,
-      })
-    }
-  } catch (err) {
-    const { response = {} } = err || {}
-    const { data = {} } = response
-    const { message } = data
-    let error = ''
-    if (typeof message === 'string') {
-      error = message
-    } else {
-      error = 'Oops, something went wrong'
-    }
+  const res = await instance.post(apiCheckoutOrderPath, orderData)
+  const { success, message } = res.data
+  if (success) {
+    showSnack({
+      variant: 'success',
+      message,
+    })
+  } else {
     showSnack({
       variant: 'error',
-      message: error,
+      message,
     })
   }
 }
