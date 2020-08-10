ce223bfaff39136ab7699a75742ab0e3c858068e
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-08-10 19:44:24 +0300

    Rework preorder with class component

--- src/components/pages/Preorder/Preorder.js
+++ src/components/pages/Preorder/Preorder.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import { Grid } from '@material-ui/core'
@@ -12,79 +12,77 @@ import Table from './Table'
 import Delivery from './Delivery'
 import Recipient from './Recipient'
 import Payment from './Payment'
-import { form } from './formConfig'
+import { createForm } from './formConfig'
 
-const Preorder = (props) => {
-  const {
-    cart,
-    user,
-    deliveryMethods,
-    paymentMethods,
-    getDeliveryMethods,
-    getPaymentMethods,
-  } = props
-  // const cartData = cart.map(item => ({
-  //   ...item.product,
-  //   count: item.count,
-  //   total: (Number(item.product.price.replace(',', '')) * item.count).toLocaleString(),
-  // }))
 
-  useEffect(() => {
+class Preorder extends Component {
+  constructor(props) {
+    super(props)
+    this.form = createForm()
+    const { getDeliveryMethods, getPaymentMethods } = props
     getDeliveryMethods()
     getPaymentMethods()
-  }, [])
+  }
 
+  render() {
+    const {
+      cart,
+      user,
+      deliveryMethods,
+      paymentMethods,
+    } = this.props
 
-  return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} lg={7}>
-        <Table cartData={cart} />
-        <Grid style={{ marginTop: 8, marginBottom: 8 }} container spacing={1}>
-          <Grid item xs={12} sm={6} xl={3}>
-            <Button variant='contained' color='primary' fullWidth onClick={form.submit}>Оформить</Button>
+    return (
+      <Grid container spacing={2}>
+        <Grid item xs={12} lg={7}>
+          <Table cartData={cart} />
+          <Grid style={{ marginTop: 8, marginBottom: 8 }} container spacing={1}>
+            <Grid item xs={12} sm={6} xl={3}>
+              <Button variant='contained' color='primary' fullWidth onClick={this.form.submit}>Оформить</Button>
+            </Grid>
+            <Grid item xs={12} sm={6} xl={3}>
+              <Button variant='contained' color='primary' disabled fullWidth>Зарезерваировать</Button>
+            </Grid>
+            <Grid item xs={12} sm={6} xl={3}>
+              <Button variant='contained' color='primary' disabled fullWidth>Выписать Счет</Button>
+            </Grid>
+            <Grid item xs={12} sm={6} xl={3}>
+              <Button variant='contained' color='primary' disabled fullWidth>Сохранить Шаблон</Button>
+            </Grid>
+            <Grid item xs={false} md={3} xl={4} />
+            <Grid item xs={12} md={6} xl={4}>
+              <Button variant='contained' color='primary' disabled fullWidth>Оплатить и Оформить</Button>
+            </Grid>
+            <Grid item xs={false} md={3} xl={4} />
           </Grid>
-          <Grid item xs={12} sm={6} xl={3}>
-            <Button variant='contained' color='primary' disabled fullWidth>Зарезерваировать</Button>
-          </Grid>
-          <Grid item xs={12} sm={6} xl={3}>
-            <Button variant='contained' color='primary' disabled fullWidth>Выписать Счет</Button>
-          </Grid>
-          <Grid item xs={12} sm={6} xl={3}>
-            <Button variant='contained' color='primary' disabled fullWidth>Сохранить Шаблон</Button>
-          </Grid>
-          <Grid item xs={false} md={3} xl={4} />
-          <Grid item xs={12} md={6} xl={4}>
-            <Button variant='contained' color='primary' disabled fullWidth>Оплатить и Оформить</Button>
-          </Grid>
-          <Grid item xs={false} md={3} xl={4} />
+        </Grid>
+        <Grid item xs={12} lg={5}>
+          <FormGroup
+            controlGroup={this.form}
+            render={({ valid, submited, submit }) => (
+              <form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Delivery
+                      deliveryMethods={deliveryMethods}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Recipient
+                      user={user}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Payment paymentMethods={paymentMethods} />
+                  </Grid>
+                </Grid>
+              </form>
+            )}
+          />
         </Grid>
       </Grid>
-      <Grid item xs={12} lg={5}>
-        <FormGroup
-          controlGroup={form}
-          render={({ valid, submited, submit }) => (
-            <form>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <Delivery
-                    deliveryMethods={deliveryMethods}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Recipient
-                    user={user}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Payment paymentMethods={paymentMethods} />
-                </Grid>
-              </Grid>
-            </form>
-          )}
-        />
-      </Grid>
-    </Grid>
-  )
+    )
+  }
 }
 
 Preorder.defaultProps = {
