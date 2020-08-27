d8f4d00da8c14efcbe672f2ae6a673fe19d3a7ae
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-08-27 17:15:11 +0300

    Add order detaols context to the store

--- src/storage/reducers/order.js
+++ src/storage/reducers/order.js
@@ -1,4 +1,4 @@
-import { SET_ORDER_LIST } from 'storage/constants/order.constants'
+import { SET_ORDER_LIST, SET_ORDER_DETAILS } from 'storage/constants/order.constants'
 
 const defaultState = {
   orders: [],
@@ -7,6 +7,13 @@ const defaultState = {
 
 export const orderList = (state = defaultState, { type, payload }) => {
   if (type === SET_ORDER_LIST) {
+    return payload
+  }
+  return state
+}
+
+export const orderDetails = (state = { }, { type, payload }) => {
+  if (type === SET_ORDER_DETAILS) {
     return payload
   }
   return state
