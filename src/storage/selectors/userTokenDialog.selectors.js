95ee8c41166d6515abcac1817c4292b6b7abf7ce
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-09-30 15:36:05 +0300

    Fixed returned value

--- src/storage/selectors/userTokenDialog.selectors.js
+++ src/storage/selectors/userTokenDialog.selectors.js
@@ -1,6 +1,6 @@
 const tokenData = state => state.userTokenDialog || {}
 
-export const open = state => tokenData(state).open
-export const token = state => tokenData(state).token
-export const code = state => tokenData(state).code
-export const userId = state => tokenData(state).userId
+export const open = state => tokenData(state).open || false
+export const token = state => tokenData(state).token || ''
+export const code = state => tokenData(state).code || ''
+export const userId = state => tokenData(state).userId || null
