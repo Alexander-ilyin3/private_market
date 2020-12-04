a0e7f3005d82c9eb8e84910715ea7d53f5696d91
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-12-04 17:14:30 +0200

    Customize search input parameters

--- src/components/parts/SearchInput/SearchInput.js
+++ src/components/parts/SearchInput/SearchInput.js
@@ -9,50 +9,88 @@ import Close from '@material-ui/icons/Close'
 import TooltipInfo from 'components/parts/TooltipInfo'
 
 
-const SearchInput = ({ onSearch, tooltipsOpened }) => {
+const SearchInput = ({
+  onSearch,
+  tooltipsOpened,
+  tooltipTitle,
+  position,
+  minValue,
+}) => {
   const [text, setText] = useState('')
   const search = (value) => {
     if (value === text) return
     setText(value)
-    if (value && value.length < 3) return
+    if (value && value.length < minValue) return
     onSearch(value)
   }
+  let justifyContent = 'flex-end'
+  switch (position) {
+    case 'start':
+      justifyContent = 'flex-start'
+      break
+    case 'end':
+      justifyContent = 'flex-end'
+      break
+    case 'center':
+      justifyContent = 'center'
+      break
+    default:
+      justifyContent = 'flex-end'
+      break
+  }
   return (
-    <TooltipInfo open={tooltipsOpened} title='Ищите товары в каталоге по артикулу или названию'>
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'end',
-          maxWidth: 350,
-        }}
-      >
-        <TextField
-          value={text}
-          onInput={(e) => {
-            const { value } = e.target
-            search(value)
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent,
+        width: '100%',
+      }}
+    >
+      <TooltipInfo open={tooltipsOpened} title={tooltipTitle}>
+        <div
+          style={{
+            maxWidth: 350,
           }}
-          InputProps={{
-            startAdornment: <Search />,
-            endAdornment: (
-              <Close
-                onClick={() => {
-                  search('')
-                }}
-                style={{ cursor: 'pointer' }}
-              />
-            ),
-          }}
-          variant='outlined'
-        />
-      </div>
-    </TooltipInfo>
+        >
+          <TextField
+            value={text}
+            onInput={(e) => {
+              const { value } = e.target
+              search(value)
+            }}
+            InputProps={{
+              startAdornment: <Search />,
+              endAdornment: (
+                <Close
+                  onClick={() => {
+                    search('')
+                  }}
+                  style={{ cursor: 'pointer' }}
+                />
+              ),
+            }}
+            variant='outlined'
+          />
+        </div>
+      </TooltipInfo>
+    </div>
   )
 }
 
 SearchInput.propTypes = {
   onSearch: PropTypes.func.isRequired,
+  tooltipsOpened: PropTypes.bool,
+  tooltipTitle: PropTypes.string,
+  position: PropTypes.string,
+  minValue: PropTypes.number,
+}
+
+SearchInput.defaultProps = {
+  tooltipsOpened: false,
+  tooltipTitle: '',
+  position: 'end',
+  minValue: 3,
 }
 
 export default SearchInput
