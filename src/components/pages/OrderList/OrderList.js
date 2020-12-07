bc3e03591f96a122aae760aad8a20a2345457005
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-12-07 12:49:51 +0200

    Fixed redirect to the order detail page

--- src/components/pages/OrderList/OrderList.js
+++ src/components/pages/OrderList/OrderList.js
@@ -70,6 +70,7 @@ const OrderList = ({ orderListInfo, getOrderList }) => {
 
   const columns = [
     { name: 'id_1c', label: 'ID 1C', options: { sort: false, display: displayed.id_1c } },
+    { name: 'id', label: 'ID', options: { sort: false, display: false, viewColumns: false } },
     { name: 'customer_name', label: 'Kлиент', options: { sort: false, display: displayed.customer_name, viewColumns: customerNameColumnViewed } },
     { name: 'created_at', label: 'Создан', options: { sort: false, display: displayed.created_at } },
     { name: 'updated_at', label: 'Обновлен', options: { sort: false, display: displayed.updated_at } },
