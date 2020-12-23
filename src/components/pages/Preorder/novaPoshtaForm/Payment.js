d7c10eb569284801deb2377e7af93833960768cc
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2020-12-23 04:35:20 +0200

    Disable prepayment payment method

--- src/components/pages/Preorder/novaPoshtaForm/Payment.js
+++ src/components/pages/Preorder/novaPoshtaForm/Payment.js
@@ -13,78 +13,85 @@ import InputAsLabel from 'components/parts/FormParts/InputAsLabel'
 
 const Delivery = ({
   paymentMethods,
-}) => (
-  <Paper style={{ padding: 16 }}>
-    <Typography variant='h5'>
-      Оплата
-    </Typography>
-    <RenderColumnItem
-      labelWeight={6}
-      label={(
-        <RFormControl
-          name='paymentType'
-          render={props => (
-            <InputAsLabel>
-              <SelectorRender
-                {...props}
-                items={paymentMethods.map(({ id, name }) => ({ value: id, label: name }))}
-              />
-            </InputAsLabel>
-          )}
-        />
-      )}
-      value={<RFormControl name='paymentAmount' render={DefaultInputRender} />}
-    />
-    <RenderColumnItem
-      labelWeight={6}
-      label={(
-        <RFormControl
-          name='deliveryPayer'
-          render={props => (
-            <InputAsLabel>
-              <SelectorRender
-                {...props}
-                items={[
-                  { value: 1, label: 'Отправитель' },
-                  { value: 2, label: 'Получатель' },
-                ]}
-              />
-            </InputAsLabel>
-          )}
-        />
-      )}
-      value={(
-        // <RFormControl
-        //   name='CODPayer'
-        //   render={props => (
-        //     <InputAsLabel>
-        //       <SelectorRender
-        //         {...props}
-        //         items={[
-        //           { value: 1, label: 'Отправитель' },
-        //           { value: 2, label: 'Получатель' },
-        //         ]}
-        //       />
-        //     </InputAsLabel>
-        //   )}
-        // />
-        <RFormControl
-          name='paymentForm'
-          render={props => (
-            <InputAsLabel>
-              <SelectorRender
-                {...props}
-                items={[
-                  { value: 1, label: 'Безналичный' },
-                  { value: 2, label: 'Наличный' },
-                ]}
-              />
-            </InputAsLabel>
-          )}
-        />
-      )}
-    />
-    {/* <RenderColumnItem
+}) => {
+  const paymentMethodsWithDisabledPrepayment = paymentMethods.map((method) => {
+    if (method.id === 2) {
+      return { ...method, disabled: true }
+    }
+    return method
+  })
+  return (
+    <Paper style={{ padding: 16 }}>
+      <Typography variant='h5'>
+        Оплата
+      </Typography>
+      <RenderColumnItem
+        labelWeight={6}
+        label={(
+          <RFormControl
+            name='paymentType'
+            render={props => (
+              <InputAsLabel>
+                <SelectorRender
+                  {...props}
+                  items={paymentMethodsWithDisabledPrepayment.map(({ id, name, disabled }) => ({ value: id, label: name, disabled }))}
+                />
+              </InputAsLabel>
+            )}
+          />
+        )}
+        value={<RFormControl name='paymentAmount' render={DefaultInputRender} />}
+      />
+      <RenderColumnItem
+        labelWeight={6}
+        label={(
+          <RFormControl
+            name='deliveryPayer'
+            render={props => (
+              <InputAsLabel>
+                <SelectorRender
+                  {...props}
+                  items={[
+                    { value: 1, label: 'Отправитель' },
+                    { value: 2, label: 'Получатель' },
+                  ]}
+                />
+              </InputAsLabel>
+            )}
+          />
+        )}
+        value={(
+          // <RFormControl
+          //   name='CODPayer'
+          //   render={props => (
+          //     <InputAsLabel>
+          //       <SelectorRender
+          //         {...props}
+          //         items={[
+          //           { value: 1, label: 'Отправитель' },
+          //           { value: 2, label: 'Получатель' },
+          //         ]}
+          //       />
+          //     </InputAsLabel>
+          //   )}
+          // />
+          <RFormControl
+            name='paymentForm'
+            render={props => (
+              <InputAsLabel>
+                <SelectorRender
+                  {...props}
+                  items={[
+                    { value: 1, label: 'Безналичный' },
+                    { value: 2, label: 'Наличный' },
+                  ]}
+                />
+              </InputAsLabel>
+            )}
+          />
+        )}
+      />
+      {/* <RenderColumnItem
       labelWeight={6}
       label={(
         <RFormControl
@@ -103,11 +110,12 @@ const Delivery = ({
         />
       )}
       value={( */}
-    <RFormControl name='insuranceAmount' render={DefaultInputRender} />
-    {/* )}
+      <RFormControl name='insuranceAmount' render={DefaultInputRender} />
+      {/* )}
     /> */}
-  </Paper>
-)
+    </Paper>
+  )
+}
 
 Delivery.defaultProps = {
   paymentMethods: [],
