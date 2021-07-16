8705192041e07691cb2569047e12e7310ae0fb07
Alexander-ilyin3 <alexander.ilyin3@gmail.com>
2021-07-16 04:53:11 +0300

    Create fake server

---
+++ fakeServer/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors')
+
+const port = 8500
+
+const corsOptions = {
+  optionsSuccessStatus: 200,
+}
+
+const app = express()
+app.use(cors({
+  optionsSuccessStatus: 200,
+}))
+
+app.post('/customer/login/', cors(corsOptions), (req, res) => {
+  res.send({
+    token_type: 'bearer',
+    access_token: '1234567',
+    success: 'true',
+  })
+})
+app.get('/customer/debt/', (req, res) => {
+  res.send({
+
+  })
+})
+
+app.get('/customer/details/', (req, res) => {
+  res.send({
+    success: true,
+    customer: {
+      customerName: 'Testovy',
+      customerLastname: 'User',
+      customerPsition: 'Student',
+      city: 'Mukhosransk',
+      address: 'Marshala Zhutkoho 15',
+      customerWebsite: 'doyky.com',
+      customerPhone: 'Nokia',
+      customerEmail: 'test@emeil.com',
+      roles: [{ name: 'admin' }, { name: 'user' }, { name: 'superAdmin' }],
+    },
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Fake server listening at http://localhost:${port}`)
+})
